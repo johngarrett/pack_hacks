772dd4b8bbe9ffa7933242de6f1026ab01ccc7ec
John Garrett <jacgarrett18@gmail.com>
2019-04-13 15:39:30 -0400

    Adding spotify urls

--- angular/pack-hacks/src/environments/environment.prod.ts
+++ angular/pack-hacks/src/environments/environment.prod.ts
@@ -7,5 +7,15 @@ export const environment = {
     projectId: "pack-hack",
     storageBucket: "pack-hack.appspot.com",
     messagingSenderId: "150932035513"
-  }
+  },
+  credentials:{
+    client:{
+      id: '67f4df5a3037476aaf5e3cf792bc44d6', //Spotify Client ID
+      secret: '75c4580629964e78912a639855832ce8', //Spotify Client Secret
+    },
+    auth: {
+      tokenHost: 'https://accounts.spotify.com',
+      tokenPath: '/api/token'
+    }
+   }
 };
