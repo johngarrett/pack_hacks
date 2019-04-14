30ecece909d8b0e9a8b5a644678bc8b179f1ebc0
John Garrett <jacgarrett18@gmail.com>
2019-04-14 02:29:26 -0400

    working on working

--- angular/js/app.js
+++ angular/js/app.js
@@ -28,98 +28,55 @@ app.config(function (SpotifyProvider) {
   // SpotifyProvider.setAuthToken('<AUTH_TOKEN>');
 });
 
- setAuthToken: function (authToken) {
+ setAuthToken = function(authToken) {
             this.authToken = authToken;
             return this.authToken;
-          },
+          };
 
-          login: function () {
-            var deferred = $q.defer();
-            var that = this;
+login = function () {
+var deferred = $q.defer();
+var that = this;
 
-            var w = 400,
-                h = 500,
-                left = (screen.width / 2) - (w / 2),
-                top = (screen.height / 2) - (h / 2);
+var w = 400,
+    h = 500,
+    left = (screen.width / 2) - (w / 2),
+    top = (screen.height / 2) - (h / 2);
 
-            var params = {
-              client_id: this.clientId,
-              redirect_uri: this.redirectUri,
-              scope: this.scope || '',
-              response_type: 'token'
-            };
+var params = {
+    client_id: this.clientId,
+    redirect_uri: this.redirectUri,
+    scope: this.scope || '',
+    response_type: 'token'
+};
 
-            var authCompleted = false;
-            var authWindow = openDialog(
-              'https://accounts.spotify.com/authorize?' + this.toQueryString(params),
-              'Spotify',
-              'menubar=no,location=no,resizable=yes,scrollbars=yes,status=no,width=' + w + ',height=' + h + ',top=' + top + ',left=' + left,
-              function () {
-                if (!authCompleted) {
-                  deferred.reject();
-                }
-              }
-            );
+var authCompleted = false;
+var authWindow = openDialog(
+    'https://accounts.spotify.com/authorize?' + this.toQueryString(params),
+    'Spotify',
+    'menubar=no,location=no,resizable=yes,scrollbars=yes,status=no,width=' + w + ',height=' + h + ',top=' + top + ',left=' + left,
+    function () {
+    if (!authCompleted) {
+        deferred.reject();
+    }
+    }
+);
 
-            function storageChanged (e) {
-              if (e.key === 'spotify-token') {
-                if (authWindow) { authWindow.close(); }
-                authCompleted = true;
+function storageChanged (e) {
+    if (e.key === 'spotify-token') {
+    if (authWindow) { authWindow.close(); }
+    authCompleted = true;
 
-                that.setAuthToken(e.newValue);
-                $window.removeEventListener('storage', storageChanged, false);
+    that.setAuthToken(e.newValue);
+    $window.removeEventListener('storage', storageChanged, false);
 
-                deferred.resolve(e.newValue);
-              }
-            }
+    deferred.resolve(e.newValue);
+    }
+}
 
-            $window.addEventListener('storage', storageChanged, false);
+$window.addEventListener('storage', storageChanged, false);
 
-            return deferred.promise;
-          }
-        };
-
-// app.run(function ($rootScope, $location, $http, $window) {
-//     'use strict';
-
-// });
-
-// app.service("SpotifyService", function ($http, $q, $window, $rootScope, $location) {
-//     'use strict';
-
-// });
-
-// app.controller("GroupController", function ($scope, $http, $routeParams) {
-//     'use strict';
-
-// });
-
-// app.controller('PlaylistController', function ($rootScope, $scope, $http, $window, $location) {
-//     'use strict';
-
-// });
-
-// app.controller('LoginController', function ($rootScope, $scope, $http, $window, $location) {
-//     'use strict';
-
-// });
-
-// app.config(['$routeProvider', function ($routeProvider, $locationProvider) {
-//     'use strict';
-
-//     $routeProvider.when('/', {
-//         templateUrl: 'html/login.html',
-//         controller: 'LoginController'
-//     }).when('/groups', {
-//         templateUrl: 'html/group.html',
-//         controller: 'GroupController'
-//     }).when("/playlists", {
-//         templateUrl: 'html/playlist.html',
-//         controller: 'PlaylistController'
-//     }).otherwise({
-//         redirectTo: '/'
-//     });
-// }]);
+return deferred.promise;
+};
 app.controller('PlaylistController', function ($rootScope, $scope, $http, $window, $location) {
     'use strict';
     $scope.songs = [
@@ -158,3 +115,21 @@ app.controller('PlaylistController', function ($rootScope, $scope, $http, $windo
         }
       ];
 });
+
+
+app.config(['$routeProvider', function ($routeProvider, $locationProvider) {
+    'use strict';
+
+    $routeProvider.when('/', {
+        templateUrl: 'html/login.html',
+        controller: 'LoginController'
+    }).when('/groups', {
+        templateUrl: 'html/group.html',
+        controller: 'GroupController'
+    }).when("/playlists", {
+        templateUrl: 'html/playlist.html',
+        controller: 'PlaylistController'
+    }).otherwise({
+        redirectTo: '/'
+    });
+}]);
